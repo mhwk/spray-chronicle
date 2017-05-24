3504ae982fd9c984fb7b48624ea349b553c3c2a6
Jurian Balledux <jurian@graciousstudios.nl>
2017-05-24 16:04:09 +0200

    Added empty test projects

---
+++ test/SprayChronicle.Persistence.Mongo.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SprayChronicle.Persistence.Mongo.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
